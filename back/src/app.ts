79c1f51b7e9c8b9f3b32922862b18a577b6289bb
Martin <martin.jokl123@gmail.com>
2026-02-25 20:33:56 +0100

    Make basic express code

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,3 +1,17 @@
-require('dotenv').config();
+import dotenv from 'dotenv';
+dotenv.config();
 
-console.log(`The bum id ${process.env.BUM}`);
+import express from 'express';
+const app = express();
+
+const port: number = Number(process.env.PORT) || 3_000;
+const start = () => {
+    try {
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port}.`);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+start();
