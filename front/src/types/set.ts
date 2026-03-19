27ecdc1bf00e22342e184a1338fa7e839232ba60
Martin <martin.jokl123@gmail.com>
2026-03-19 19:06:53 +0100

    dd types for fullset and flashcard

--- front/src/types/set.ts
+++ front/src/types/set.ts
@@ -1,7 +1,18 @@
+import type { Flashcard } from "./flashcards";
+
 export type Set = {
   name: string;
   description: string | null | undefined;
   creatorName: string;
   id: string;
   likes: number;
+}
+export type FullSet = {
+  name: string,
+  description: string | null | undefined,
+  id: string,
+  likes: number,
+  createdBy: string,
+  creatorName: string,
+  flashcards: Flashcard[]
 }
