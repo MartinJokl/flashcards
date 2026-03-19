a9c7c2f8dc8aa8998a44e4d8bde98b5e9b791369
Martin <martin.jokl123@gmail.com>
2026-03-19 18:49:37 +0100

    Make setDisplay look better

--- front/src/components/SetDisplay.tsx
+++ front/src/components/SetDisplay.tsx
@@ -4,10 +4,14 @@ import './SetDisplay.css'
 function SetDisplay({ set }: { set: Set }) {
   return (
     <button className="set-display">
-      <h3 className="set-display-name">{set.name}</h3>
-      <p>{set.description || 'No description'}</p>
-      <div>{set.creatorName}</div>
-      <div>{set.likes} likes</div>
+      <div className="set-display-name-description-container">
+        <h3 className="set-display-name">{set.name}</h3>
+        <p>{set.description || 'No description'}</p>
+      </div>
+      <div className="set-display-creator-likes-container">
+        <div>{set.creatorName}</div>
+        <div>{set.likes} likes</div>
+      </div>
     </button>
   )
 }
